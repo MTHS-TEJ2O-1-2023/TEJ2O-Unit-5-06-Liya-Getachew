37506c08780ca68de692c9758ee2fad801da643c
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2023-10-26 17:10:01 +0000

    fixed py

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -5,59 +5,31 @@ This module is a Micro:bit MicroPython program detects distance with sonar.
 """
 
 from microbit import *
+from machine import time_pulse_us
 
 
-class HCSR04:
-    # this class abstracts out the functionality of the HC-SR04 and
-    #   returns distance in mm
-    # Trig: pin 1
-    # Echo: pin 2
-    def __init__(self, tpin=pin1, epin=pin2, spin=pin13):
-        self.trigger_pin = tpin
-        self.echo_pin = epin
-        self.sclk_pin = spin
-
-    def distance_mm(self):
-        spi.init(
-            baudrate=125000,
-            sclk=self.sclk_pin,
-            mosi=self.trigger_pin,
-            miso=self.echo_pin,
-        )
-        pre = 0
-        post = 0
-        k = -1
-        length = 500
-        resp = bytearray(length)
-        resp[0] = 0xFF
-        spi.write_readinto(resp, resp)
-        # find first non zero value
-        try:
-            i, value = next((ind, v) for ind, v in enumerate(resp) if v)
-        except StopIteration:
-            i = -1
-        if i > 0:
-            pre = bin(value).count("1")
-            # find first non full high value afterwards
-            try:
-                k, value = next(
-                    (ind, v)
-                    for ind, v in enumerate(resp[i : length - 2])
-                    if resp[i + ind + 1] == 0
-                )
-                post = bin(value).count("1") if k else 0
-                k = k + i
-            except StopIteration:
-                i = -1
-        dist = -1 if i < 0 else round(((pre + (k - i) * 8.0 + post) * 8 * 0.172) / 2)
-        return dist
-
-
-# variables
-sonar = HCSR04
 display.show(Image.DUCK)
+
+# choose pins
+trig = pin1
+echo = pin2
+
+# setup
+trig.write_digital(0)
+echo.read_digital()
 
 while True:
     if button_a.is_pressed():
-        display.show(sonar.distance_mm() / 10)
+        # output
+        trig.write_digital(1)
+        trig.write_digital(0)
+
+        # Measure the echo pulse in miroseconds then convert to seconds
+        micros = time_pulse_us(echo, 1)
+        t_echo = micros / 1000000
+
+        # Calculate distance in cm
+        dist_cm = (t_echo / 2) * 34300
+        display.scroll(str(int(dist_cm)))
+
         sleep(300)
